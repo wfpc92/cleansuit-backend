239d0700e42d46adc6ace76f90c17b2b4a915b6b
Mateo TibaPalacios <nestormateo@gmail.com>
2017-01-07 00:25:27 -0500

    fix(db): mongoose configured and working with promised app

--- app.js
+++ app.js
@@ -46,122 +46,122 @@ const passportConfig = require('./config/passport');
 const app = express();
 
 /**
+ * Run the app after MongoDB.
+ */
+function runapp() {
+    /**
+     * Express configuration.
+     */
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'pug');
+    app.use(expressStatusMonitor());
+    app.use(compression());
+    app.use(sass({
+      src: path.join(__dirname, 'public'),
+      dest: path.join(__dirname, 'public')
+    }));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(expressValidator());
+    app.use(session({
+      resave: true,
+      saveUninitialized: true,
+      secret: process.env.SESSION_SECRET,
+      store: new MongoStore({
+        url: process.env.MONGODB_URI,
+        autoReconnect: true
+      })
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(flash());
+    app.use((req, res, next) => {
+      if (req.path === '/api/upload') {
+        next();
+      } else {
+        lusca.csrf()(req, res, next);
+      }
+    });
+    app.use(lusca.xframe('SAMEORIGIN'));
+    app.use(lusca.xssProtection(true));
+    app.use((req, res, next) => {
+      res.locals.user = req.user;
+      next();
+    });
+    app.use((req, res, next) => {
+      // After successful login, redirect back to the intended page
+      if (!req.user &&
+          req.path !== '/login' &&
+          req.path !== '/signup' &&
+          !req.path.match(/^\/auth/) &&
+          !req.path.match(/\./)) {
+        req.session.returnTo = req.path;
+      } else if (req.user &&
+          req.path == '/account') {
+        req.session.returnTo = req.path;
+      }
+      next();
+    });
+    app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+
+    /**
+     * Primary app routes.
+     */
+    app.get('/', homeController.index);
+    app.get('/login', userController.getLogin);
+    app.post('/login', userController.postLogin);
+    app.get('/logout', userController.logout);
+    app.get('/forgot', userController.getForgot);
+    app.post('/forgot', userController.postForgot);
+    app.get('/reset/:token', userController.getReset);
+    app.post('/reset/:token', userController.postReset);
+    app.get('/signup', userController.getSignup);
+    app.post('/signup', userController.postSignup);
+    app.get('/contact', contactController.getContact);
+    app.post('/contact', contactController.postContact);
+    app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
+    app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
+    app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
+    app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
+    app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
+
+    /**
+     * API examples routes.
+     */
+    app.get('/api/upload', apiController.getFileUpload);
+    app.post('/api/upload', upload.single('myFile'), apiController.postFileUpload);
+
+    /**
+     * OAuth authentication routes. (Sign in)
+     */
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
+    app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
+      res.redirect(req.session.returnTo || '/');
+    });
+    app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
+    app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+      res.redirect(req.session.returnTo || '/');
+    });
+
+    /**
+     * Error Handler.
+     */
+    app.use(errorHandler());
+
+    /**
+     * Start Express server.
+     */
+    app.listen(app.get('port'), () => {
+      console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+      console.log('  Press CTRL-C to stop\n');
+    });
+}
+
+/**
  * Connect to MongoDB.
  */
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
-mongoose.connection.on('error', () => {
-  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
-  process.exit();
-});
-
-/**
- * Express configuration.
- */
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(expressStatusMonitor());
-app.use(compression());
-app.use(sass({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public')
-}));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressValidator());
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: process.env.SESSION_SECRET,
-  store: new MongoStore({
-    url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
-    autoReconnect: true
-  })
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-app.use((req, res, next) => {
-  if (req.path === '/api/upload') {
-    next();
-  } else {
-    lusca.csrf()(req, res, next);
-  }
-});
-app.use(lusca.xframe('SAMEORIGIN'));
-app.use(lusca.xssProtection(true));
-app.use((req, res, next) => {
-  res.locals.user = req.user;
-  next();
-});
-app.use((req, res, next) => {
-  // After successful login, redirect back to the intended page
-  if (!req.user &&
-      req.path !== '/login' &&
-      req.path !== '/signup' &&
-      !req.path.match(/^\/auth/) &&
-      !req.path.match(/\./)) {
-    req.session.returnTo = req.path;
-  } else if (req.user &&
-      req.path == '/account') {
-    req.session.returnTo = req.path;
-  }
-  next();
-});
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
-
-/**
- * Primary app routes.
- */
-app.get('/', homeController.index);
-app.get('/login', userController.getLogin);
-app.post('/login', userController.postLogin);
-app.get('/logout', userController.logout);
-app.get('/forgot', userController.getForgot);
-app.post('/forgot', userController.postForgot);
-app.get('/reset/:token', userController.getReset);
-app.post('/reset/:token', userController.postReset);
-app.get('/signup', userController.getSignup);
-app.post('/signup', userController.postSignup);
-app.get('/contact', contactController.getContact);
-app.post('/contact', contactController.postContact);
-app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
-app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
-app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
-app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
-app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
-
-/**
- * API examples routes.
- */
-app.get('/api/upload', apiController.getFileUpload);
-app.post('/api/upload', upload.single('myFile'), apiController.postFileUpload);
-
-/**
- * OAuth authentication routes. (Sign in)
- */
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
-  res.redirect(req.session.returnTo || '/');
-});
-app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
-  res.redirect(req.session.returnTo || '/');
-});
-
-/**
- * Error Handler.
- */
-app.use(errorHandler());
-
-/**
- * Start Express server.
- */
-app.listen(app.get('port'), () => {
-  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
-  console.log('  Press CTRL-C to stop\n');
-});
+require('./config/database')(mongoose, runapp);
 
 module.exports = app;
---
+++ config/database.js
@@ -0,0 +1,40 @@
+const autoIncrement = require('mongoose-auto-increment');
+const chalk = require('chalk');
+
+module.exports = (mongoose, done) => {
+    mongoose.Promise = global.Promise;
+
+    let dbs = [
+        process.env.MONGOLOCAL_URI,
+        process.env.MONGOSERVER_URI,
+    ];
+
+    var dbURI = dbs.pop();
+
+    mongoose.connection.on('open', () => {
+        process.env.MONGODB_URI = dbURI;
+        console.log(`%s Conectado a: %s`, chalk.green('✓'), chalk.green(dbURI));
+        return done();
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log(`%s No se pudo conectar a: %s`, chalk.red('✗'), chalk.red(dbURI));
+        dbURI = dbs.pop();
+    	if (!dbURI) {
+    		throw err;
+    	}
+    	conectar(dbURI);
+    });
+
+    function conectar(dbURI) {
+    	try {
+    		mongoose.connect(dbURI);
+    		autoIncrement.initialize(mongoose.connection);
+    		console.log(`  Intentando conexión a MongoDB, esperando respuesta...`);
+    	} catch(err) {
+    		console.log(`  Falló al conectar a: ${dbURI}`, err.message);
+    	}
+    }
+
+    conectar(dbURI);
+};
