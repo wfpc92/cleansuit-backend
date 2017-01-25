8a2c5e7dae62b5cec129f0daef2c1695bc9725c5
Mateo TibaPalacios <nestormateo@gmail.com>
2017-01-25 18:01:11 -0500

    ref(account): renamed controller

--- controllers/index.js
+++ controllers/index.js
@@ -3,12 +3,12 @@
 module.exports = (app) => {
   const router = require('express').Router()
 
-  router.use(require('./auth')(app))
-  router.use(require('./user')(app))
   router.use(require('./pages')(app))
   router.use(require('./contact')(app))
-  router.use(require('./api')(app))
+  router.use(require('./auth')(app))
+  router.use(require('./account')(app))
   router.use(require('./dashboard')(app))
+  router.use(require('./api')(app))
 
   return router
 }
