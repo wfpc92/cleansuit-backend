5932bf60d10a2d71a9e2cb11c51718e2e7e33959
Mateo TibaPalacios <nestormateo@gmail.com>
2017-01-24 22:33:17 -0500

    add(dash.user): add user entity to the dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,9 +3,20 @@ var dashApp = angular.module('dashCleanSuit', ['ng-admin']);
 // declare a function to run when the module bootstraps (during the 'config' phase)
 dashApp.config(['NgAdminConfigurationProvider', function (nga) {
     // create an admin application
-    var admin = nga.application('Panel | CleanSuit');
-    // more configuration here later
-    // ...
+    var admin = nga.application('Panel | CleanSuit')
+                   .baseApiUrl('http://localhost:3000/rest/');
+
+    // create a User entity
+    var user = nga.entity('user');
+    // set the fields of the user entity list view
+    user.listView().fields([
+        nga.field('profile.name'),
+        nga.field('email'),
+        nga.field('rol'),
+    ]);
+    // add the user entity to the admin application
+    admin.addEntity(user);
+
     // attach the admin application to the DOM and execute it
     nga.configure(admin);
 }]);
