c5e9848a35fdf3130713e7dbf634154a951a43a2
Mateo TibaPalacios <nestormateo@gmail.com>
2017-03-03 23:36:14 -0500

    fix(misc): details adjusted for the presentation

--- models/ProduMoves.js
+++ models/ProduMoves.js
@@ -4,6 +4,8 @@ module.exports = (app) => {
   const mongoose = require('mongoose');
   const restful = require('node-restful');
 
+  const Productos = mongoose.model('Productos');
+
   const TIPOS = ['compra', 'venta', 'ajuste'];
 
   const movesSchema = new mongoose.Schema({
@@ -20,6 +22,47 @@ module.exports = (app) => {
     ref: String,  // comentario
   }, {
     timestamps: true
+  });
+
+  movesSchema.post('save', function() {
+    var move = this;
+    Productos.findOne({ '_id': move.producto_id }, function (err, product) {
+      if (!product.stock) {
+        product.stock = 0;
+      }
+      if (product) {
+        switch (move.tipo) {
+          case 'ajuste':
+            product.stock = move.cantidad;
+            break;
+          case 'compra':
+            product.stock += move.cantidad;
+            break;
+          case 'venta':
+            product.stock -= move.cantidad;
+            break;
+        }
+        product.save();
+      }
+    })
+  });
+
+  movesSchema.post('findOneAndRemove', function(move) {
+    Productos.findOne({ '_id': move.producto_id }, function (err, product) {
+      if (product) {
+        switch (move.tipo) {
+          case 'ajuste':
+            break;
+          case 'compra':
+            product.stock -= move.cantidad;
+            break;
+          case 'venta':
+            product.stock += move.cantidad;
+            break;
+        }
+        product.save();
+      }
+    })
   });
 
   /**
---
+++ public/js/ng_es-co.js
@@ -0,0 +1,125 @@
+'use strict';
+angular.module("ngLocale", [], ["$provide", function($provide) {
+var PLURAL_CATEGORY = {ZERO: "zero", ONE: "one", TWO: "two", FEW: "few", MANY: "many", OTHER: "other"};
+$provide.value("$locale", {
+  "DATETIME_FORMATS": {
+    "AMPMS": [
+      "a.m.",
+      "p.m."
+    ],
+    "DAY": [
+      "Domingo",
+      "Lunes",
+      "Martes",
+      "Mi\u00e9rcoles",
+      "Jueves",
+      "Viernes",
+      "S\u00e1bado"
+    ],
+    "ERANAMES": [
+      "Antes de Cristo",
+      "Despu\u00e9s de Cristo"
+    ],
+    "ERAS": [
+      "a.C.",
+      "d.C."
+    ],
+    "FIRSTDAYOFWEEK": 6,
+    "MONTH": [
+      "Enero",
+      "Febrero",
+      "Marzo",
+      "Abril",
+      "Mayo",
+      "Junio",
+      "Julio",
+      "Agosto",
+      "Septiembre",
+      "Octubre",
+      "Noviembre",
+      "Diciembre"
+    ],
+    "SHORTDAY": [
+      "Dom",
+      "Lun",
+      "Mar",
+      "Mi\u00e9",
+      "Jue",
+      "Vie",
+      "S\u00e1b"
+    ],
+    "SHORTMONTH": [
+      "Ene",
+      "Feb",
+      "Mar",
+      "Abr",
+      "May",
+      "Jun",
+      "Jul",
+      "Ago",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dic"
+    ],
+    "STANDALONEMONTH": [
+      "Enero",
+      "Febrero",
+      "Marzo",
+      "Abril",
+      "Mayo",
+      "Junio",
+      "Julio",
+      "Agosto",
+      "Septiembre",
+      "Octubre",
+      "Noviembre",
+      "Diciembre"
+    ],
+    "WEEKENDRANGE": [
+      5,
+      6
+    ],
+    "fullDate": "EEEE, d 'de' MMMM 'de' y",
+    "longDate": "d 'de' MMMM 'de' y",
+    "medium": "d/MM/y h:mm:ss a",
+    "mediumDate": "d/MM/y",
+    "mediumTime": "h:mm:ss a",
+    "short": "d/MM/yy h:mm a",
+    "shortDate": "d/MM/yy",
+    "shortTime": "h:mm a"
+  },
+  "NUMBER_FORMATS": {
+    "CURRENCY_SYM": "$",
+    "DECIMAL_SEP": ",",
+    "GROUP_SEP": ".",
+    "PATTERNS": [
+      {
+        "gSize": 3,
+        "lgSize": 3,
+        "maxFrac": 3,
+        "minFrac": 0,
+        "minInt": 1,
+        "negPre": "-",
+        "negSuf": "",
+        "posPre": "",
+        "posSuf": ""
+      },
+      {
+        "gSize": 3,
+        "lgSize": 3,
+        "maxFrac": 2,
+        "minFrac": 2,
+        "minInt": 1,
+        "negPre": "-\u00a4",
+        "negSuf": "",
+        "posPre": "\u00a4",
+        "posSuf": ""
+      }
+    ]
+  },
+  "id": "es-co",
+  "localeID": "es_CO",
+  "pluralCat": function(n, opt_precision) {  if (n == 1) {    return PLURAL_CATEGORY.ONE;  }  return PLURAL_CATEGORY.OTHER;}
+});
+}]);
